5f00fab5f958c6893623ae448e7eaeb23dd70e5d
Olajide-Ibukunoluwa-Temitope <ibk12mails@gmail.com>
2018-02-23 14:15:16 +0100

    able to go into directories but unable to reverse process yet

--- safe_rm.sh
+++ safe_rm.sh
@@ -13,52 +13,46 @@ trashPath="$HOME/.Trash_Saferm"
 removeFiles(){
   # check if item is a file
 
-  if [[ -f "$1" ]];
-  then
+  # if [[ -f "$1" ]];
+  # then
       #ask for permission to remove file
-          read -p "remove $currentdir/$object? " response
-          if [[ $response == Y* ]] || [[ $response == y* ]]
-          then
+          # read -p "remove $currentdir/$object? " response
+          # if [[ $response == Y* ]] || [[ $response == y* ]]
+          # then
               #remove file
-                # mv "$presentItem" $trashPath
-               echo "$currentdir/$object has been removed"
-          else
-              #file isn't remove
-                  echo "$currentdir/$object not removed"
-          fi
-   fi
+              echo " "
+                 # mv "$1" $trashPath
+               # echo "$1 has been removed"
+          # else
+          #     #file isn't remove
+          #         echo "$currentdir/$object not removed"
+          # fi
+   # fi
 }
 
- # checkIfDirectoryIsEmpty(){
-#   echo "============================================"
-#   echo "in checkIfDirectoryIsEmpty";
-#
-#           dirContentsCheck=$(ls -l "$1" | sort -k1,1 | awk -F " " '{print $NF}' | sed -e '$ d' | wc -l | xargs)
-#           #do a check on the the directory
-#           if [[ $dirContentsCheck -gt 0 ]]
-#           then
-#                echo "Directory not empty"
-#             # recursiveActionInDirectory $1
-#
-#           elif [[ $dirContentsCheck -eq 0 ]]
-#           then
-#               # echo ""
-#               # read -p "remove directory $1? " response
-#               # if [[ $response == Y* ]] || [[ $response == y* ]]
-#               # then
-#                   echo "$1 removed"
-#                   # mv "$itemPath" $trashPath
-#               # fi
-#           fi
-#         # else
-#         #     echo "Directory not examined"
-#         # fi
-#
-# }
+ backToRootDir(){
+   # echo "=============================================="
+   # echo " THIS IS BACK TO DIR!!!!!!!!! "
+   # echo "=============================================="
+  dirContentsCheck=$(ls -l "$1" | sort -k1,1 | awk -F " " '{print $NF}' | sed -e '$ d' | wc -l | xargs)
+
+    read -p "remove $1? " response
+    if [[ $response == Y* || $response == y* ]] && [[ $dirContentsCheck -eq 0 || $dirContentsCheck -gt 0 ]]
+    then
+      removeFiles $1
+      # backToRootDir $currentdir
+      # echo "PRESENT SIR!!!!!!"
+        echo "$1 removed"
+    else
+        echo "$1 not removed"
+    fi
+}
+
 
 recursiveActionInDirectory(){
-    # echo "============================================"
-    # echo "in recursiveActionInDirectory";
+     # echo "============================================"
+     # echo "in recursiveActionInDirectory"
+     # echo "============================================"
 
     currentdir="$1"
     totalItems=$(ls -l "$currentdir" | sort -k1,1  | awk -F " " '{print $NF}' | sed -e '$ d')
@@ -71,16 +65,23 @@ recursiveActionInDirectory(){
               then
                   removeFiles $currentdir/$object
               else
-                  read -p "do you want to examine files in $currentdir/$object? " response
+                  read -p "examine files in directory $currentdir/$object? " response
                   if [[ $response == Y* ]] || [[ $response == y* ]]
                   then
                     currentdir="$1"
                     dirContentsCheck=$(ls -l "$currentdir/$object" | sort -k1,1 | awk -F " " '{print $NF}' | sed -e '$ d' | wc -l | xargs)
-                      if [[ -d "$currentdir/$object" && $dirContentsCheck -gt 0 ]]
+                      if [[ -d "$currentdir/$object" ]]
                       then
+                        # currentdir=$currentdir
                            recursiveActionInDirectory $currentdir/$object
-                      else
-                        echo "$currentdir/$object removed"
+                           # currentdir=
+                           backToRootDir $currentdir
+                      # elif [[ -d "$currentdir/$object" && $dirContentsCheck -eq 0 ]]
+                      # then
+                      #   # echo "$currentdir/$object "
+                      #     echo "EMPTY!!!!!"
+                      #    backToRootDir $currentdir/$object
+                      #   # echo "remove $currentdir/$object? "
                       fi
                   else
                       echo "$currentdir/$object not examined"
@@ -90,7 +91,7 @@ recursiveActionInDirectory(){
 
 }
 
-
+#===========================================================================================================================================
 
 # FOR FILES
 if [[ -f "$presentItem" ]];
@@ -102,12 +103,14 @@ fi
 if [[ -d "$1" ]];
 then
   # currentdir="$1"
-  read -p "do you want to examine files in $1? " response
+  read -p "examine files in directory $1? " response
 
      if [[ $response == Y* ]] || [[ $response == y* ]]
      then
     #first do a check to know if file is empty or not
         recursiveActionInDirectory $1
+        # backToRootDir $1/$object
+        backToRootDir $1
      else
          echo "$1 not examined"
      fi
